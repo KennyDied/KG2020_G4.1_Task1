3db9d6bcc7e35acabc2b55a8578e853fcbb15437
thekennydied@mail.ru <12211234mc123mc>
2020-09-12 23:54:45 +0300

    add random color generation

--- src/Tree.java
+++ src/Tree.java
@@ -20,11 +20,23 @@ public class Tree implements Drawable {
             int randomHighPosOfStump = (int)Math.floor((Math.random() * ((height / 2) - 65 * height / 100)) + 65 * height / 100);
             int randomLowPosOfStump = (int)Math.floor((Math.random() * ((3 * height / 4) - 7 * height / 10)) + 7 * height / 10);
 
-            g.setColor(new Color(160, 82, 45));
+
+
+            int randomLeavesColorR = (int)(Math.random() * 205);
+            int randomLeavesColorG = 255;
+            int randomLeavesColorB = (int)(Math.random() * 140);
+
+
+            int randomStumpColorR = 255;
+            int randomStumpColorG = (int)(Math.random() * 150);
+            int randomStumpColorB = (int)(Math.random() * 1);
+
+
+            g.setColor(new Color(randomStumpColorR, randomStumpColorG, randomStumpColorB));
             g.setStroke(new BasicStroke(randomWidthOfStump));
             g.drawLine(distanceBetweenTrees * i + width / 40, randomHighPosOfStump, distanceBetweenTrees * i + width / 40,  randomLowPosOfStump);
 
-            g.setColor(new Color(34, 139, 34));
+            g.setColor(new Color(randomLeavesColorR, randomLeavesColorG, randomLeavesColorB));
             g.fillOval(distanceBetweenTrees * i + width / 40 - 5 * randomWidthOfStump / 2, randomHighPosOfStump - 7 * randomWidthOfStump / 2, randomWidthOfStump * 5, randomWidthOfStump * 7);
         }
     }
